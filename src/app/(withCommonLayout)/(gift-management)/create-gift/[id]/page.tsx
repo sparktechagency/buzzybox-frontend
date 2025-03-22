b233c1edec9c299f73fe293c1147ae574142a517
Rahad Ullah <myemail.abc20@gmail.com>
2025-03-22 18:02:08 +0600

    Implemented updating card background image

--- src/app/(withCommonLayout)/(gift-management)/create-gift/[id]/page.tsx
+++ src/app/(withCommonLayout)/(gift-management)/create-gift/[id]/page.tsx
@@ -5,10 +5,14 @@ import { Button, Drawer } from 'antd';
 import { useState } from 'react';
 import { MenuOutlined } from '@ant-design/icons';
 import { useParams } from 'next/navigation';
+import { useGetSingleGiftCardQuery } from '@/redux/features/website/gift-card/giftCardApi';
 
 const CreateGiftPage = () => {
       const params = useParams();
       const id = Array.isArray(params?.id) ? params.id[0] : params?.id;
+
+      const { data } = useGetSingleGiftCardQuery({ id });
+      const giftCard = data?.data;
 
       const [isDrawerOpen, setIsDrawerOpen] = useState(false);
       return (
@@ -23,7 +27,7 @@ const CreateGiftPage = () => {
                   </div>
                   <div className="flex h-full justify-center gap-6 py-20">
                         <div className="w-[35%] hidden md:block ">
-                              <Sidebar />
+                              <Sidebar id={giftCard?._id} />
                         </div>
                         <Drawer
                               title="Configuration"
@@ -33,7 +37,7 @@ const CreateGiftPage = () => {
                               width="100%"
                               className="md:hidden"
                         >
-                              <Sidebar />
+                              <Sidebar id={giftCard?._id} />
                         </Drawer>
                         <div className="md:w-[65%] w-full max-w-[792px] min-h-[734px] relative">
                               <Preview id={id} />
